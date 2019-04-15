c29a75077563c70879e8e0406b83ce55a9eb3ac2
Mateus Koppe <mateus.koppe@gmail.com>
2019-04-15 16:18:56 -0300

    Fix alg c

--- ED/list3ex2/alg.c
+++ ED/list3ex2/alg.c
@@ -5,38 +5,39 @@
 int main(void) {
   char letter, *p_msg, **p_lines;
   int *p_msg_size;
-  int i = 0, j = 0;
+  int i = 0;
   int lines_amount = 1, actual_line = 0;
 
   p_msg = malloc(0);
-  p_lines = malloc(sizeof(int));
+  p_lines = (char **)  malloc(sizeof(int));
   p_lines[0] = p_msg;
-  p_msg_size = malloc(sizeof(int));
+  p_msg_size = (int *) malloc(sizeof(int*));
   p_msg_size[0] = 0;
   while (scanf("%c", &letter) != EOF) {
     if (!strncmp(&letter, "\n", 1)) {
       lines_amount++;
       actual_line++;
       p_msg = malloc(0);
-      p_msg_size = realloc(p_msg_size, lines_amount * sizeof(int));
+      p_msg_size = (int *) realloc(p_msg_size, lines_amount * sizeof(int*));
       p_msg_size[actual_line] = 0;
-      p_lines = realloc(p_lines, lines_amount * sizeof(int));
-      p_lines[actual_line] = &p_msg;
+      p_lines = (char **) realloc(p_lines, lines_amount * sizeof(int*));
+      p_lines[actual_line] = &(*p_msg);
     } else {
       p_msg_size[actual_line]++;
-      p_msg = realloc(p_msg, p_msg_size[actual_line] * sizeof(char));
+      p_msg = realloc(p_msg, p_msg_size[actual_line] * sizeof(char*));
       p_lines[actual_line] = p_msg;
       p_msg[p_msg_size[actual_line] - 1] = letter;
     }
   }
 
+  printf("\n");
   for(i = 0; i < lines_amount; i++){
     printf("%s", p_lines[i]);
+    printf("\n");
     free(p_lines[i]);
   }
 
   free(p_msg_size);
   free(p_lines);
-  return 0;
 }
 
