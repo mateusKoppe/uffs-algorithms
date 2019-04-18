4311e46268f9c6f9841d4e16225da3867f3b3ed7
Mateus Koppe <mateus.koppe@gmail.com>
2019-04-18 16:32:01 -0300

    Binary search

---
+++ ED/binary-search/alg.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+
+int binary_search(int values[], int start, int last_index, int target) {
+  int pivot = (start + last_index) / 2;
+  int actual_item = values[pivot];
+  
+  if(actual_item == target) {
+    return pivot;
+  } else if ((last_index == 0) || start == last_index){
+    return -1; 
+  } else if (actual_item > target) {
+    return binary_search(values, start, pivot, target);
+  } else if (actual_item < target) {
+    return binary_search(values, pivot + 1, last_index, target);
+  }
+  return -1;
+}
+
+int main(void){
+  int array[] = {1, 3, 5, 6, 7, 8, 10, 12, 15, 17, 18, 19, 20, 21};
+  int size = sizeof(array) / sizeof(array[0]);
+  int result;
+  result = binary_search(array, 0, size - 1, array[0]);
+  return 0;
+}
