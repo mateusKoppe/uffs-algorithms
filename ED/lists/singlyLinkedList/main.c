51e7cdc36bc54b82a46288b7a8d6ecba16c7f99f
Mateus Koppe <mateus.koppe@gmail.com>
2019-05-28 12:19:20 -0300

    Add better feedbacks for actions

--- ED/lists/singlyLinkedList/main.c
+++ ED/lists/singlyLinkedList/main.c
@@ -9,15 +9,22 @@ int main(void) {
   sllist_init(&list);
   stack s;
   stack_init(&s);
+  printf("[i] List Insert\n");
+  printf("[e] List Erase\n");
+  printf("[p] List Print\n");
+  printf("[u] Stack pUsh\n");
+  printf("[o] Stack pOp\n");
   while (scanf(" %c", &option) != EOF) {
     switch(option) {
       case 'i':
         scanf(" %d", &i);
         sllist_insert(&list, i);
+        printf("Item added in list: %d\n", i);
         break;
       case 'e':
         scanf(" %d", &i);
         sllist_erase(&list, i);
+        printf("Item erased in list: %d\n", i);
         break;
       case 'p':
         sllist_print(&list);
@@ -25,10 +32,11 @@ int main(void) {
       case 'u':
         scanf(" %d", &i);
         stack_push(&s, i);
+        printf("Item added in stack: %d\n", i);
         break;
       case 'o':
         i = stack_pop(&s);
-        printf("Item deleted: %d\n", i);
+        printf("Item deleted in stack: %d\n", i);
         break;
     }
   }
