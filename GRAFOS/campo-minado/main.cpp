c88aba73573b6039e4f0b41050b637511f39ec70
Mateus Koppe <mateus.koppe@gmail.com>
2020-03-09 01:02:38 -0300

    Count bombs

--- GRAFOS/campo-minado/main.cpp
+++ GRAFOS/campo-minado/main.cpp
@@ -5,35 +5,104 @@
 
 using namespace std;
 
-struct block {
-    bool isBomb = false;
+struct field {
+    bool isBomb;
+    int bombsAround = 0;
 };
 
-int rows, columns, bombsAmount;
-vector<vector<block>> field;
+class board {
+    public:
+        int rows, columns, bombsAmount;
+        vector<vector<field>> fields;
+
+        void generate () {
+            fields.assign(rows, vector<field>(columns, field()));
+            while (!generateBombs()) {
+                cout << "The field size doest support this amount of bombs, enter again please.\n"
+                    << "Bombs: ";
+                cin >> bombsAmount;
+            }
+            calcBombs();
+        }
+
+        int getSize () {
+            return rows * columns;
+        }
+
+        void render () {
+            cout << "\n";
+            for (int y = 0; y < rows; y++) {
+                for (int x = 0; x < columns; x++) {
+                    if (fields[y][x].isBomb) {
+                        cout << "B ";    
+                    } else { 
+                        cout << fields[y][x].bombsAround << " ";
+                    }
+                }
+                cout << "\n";
+            }
+        }
+
+    private:
+        bool generateBombs () {
+            if (bombsAmount >= getSize()) {
+                return false;
+            }
+
+            srand((unsigned)time(NULL));
+
+            int bombsInMap = 0;
+            while (bombsInMap < bombsAmount) {
+                int y = rand() % rows;
+                int x = rand() % columns;
+                if (fields[y][x].isBomb) {
+                    continue;
+                }
+                fields[y][x].isBomb = true;
+                bombsInMap++;
+            }
+
+            return true;
+        }
+
+        void calcBombs () {
+            for (int y = 0; y < rows; y++) {
+                for (int x = 0; x < columns; x++) {
+                    fields[y][x].bombsAround = getBombsAroundField(y, x);
+                }
+            }
+        }
+
+        int getBombsAroundField (int fieldY, int fieldX) {
+            vector<int> range = {-1, 0, 1};
+            int bombsAround = 0;
+            field actualField = fields[fieldY][fieldX];
+            for (int y : range) {
+                for (int x : range) {
+                    int targetX = x + fieldX;
+                    int targetY = y + fieldY;
+                    if (targetY < 0 || targetX < 0 || targetY >= rows || targetX >= columns) continue;
+                    if ((y == 0 && x == 0)) continue;
+                    field targetField = fields[targetY][targetX];
+                    if (targetField.isBomb) {
+                        bombsAround++;
+                    }
+                }
+            }
+            return bombsAround;
+        }
+};
 
 int main () {
+    board gameBoard;
+
     cout << "Rows: ";
-    cin >> rows;
+    cin >> gameBoard.rows;
     cout << "Columns: ";
-    cin >> columns;
+    cin >> gameBoard.columns;
     cout << "Bombs: ";
-    cin >> bombsAmount;
+    cin >> gameBoard.bombsAmount;
 
-    field.assign(rows, vector<block>(columns, block()));
-
-    srand((unsigned)time(NULL));
-
-    for (int i = 0; i < bombsAmount; i++) {
-        int y = rand() % rows;
-        int x = rand() % columns;
-        field[y][x].isBomb = true;
-    }
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            cout << field[i][j].isBomb << " ";
-        }
-        cout << "\n";
-    }
+    gameBoard.generate();
+    gameBoard.render();
 }
