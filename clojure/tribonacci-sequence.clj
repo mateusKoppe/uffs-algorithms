5dd1ad5b0e5ce7efe515725fe8e7299b14dcf1f5
Mateus Koppe <mateus.koppe@gmail.com>
2020-12-19 21:27:46 -0300

    Tribonacci

--- clojure/tribonacci-sequence.clj
+++ clojure/tribonacci-sequence.clj
@@ -1,9 +1,13 @@
 ; https://www.codewars.com/kata/556deca17c58da83c00002db/train/clojure
 
-(defn tribonacci [[a b c] n]
-    (reduce #(conj % %2) [] (range n))
+(defn tribonacci [[a b c :as items] n]
+    (reduce
+        (fn [list i] (conj list 
+            (if (< i 3)
+                (get items i)
+                (apply + (map #(get list (- i % 1)) (range 3))))))
+        []
+        (range n))
 )
-
-(range 0)
 
 (tribonacci [1 1 1] 10)
